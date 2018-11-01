a42c6fe2945d4993315a7123d757421f17da9680
Ikem43 <ikem43@gmail.com>
2018-11-01 21:51:31 +0100

    Stiil Working on responsiveness

---
+++ VENDEE_WEB/js/main.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".type-it", {
+    strings: ['groceries', 'cosmetics', 'beverages'],
+    typeSpeed: 150,
+    backSpeed: 25,
+    cursorChar: '|',
+    shuffle: false,
+    smartBackspace: true,
+    fadeOut: false,
+    loop: true
+});
---
+++ VENDEE_WEB/main.js
@@ -0,0 +1,6 @@
+new TypeIt('.wordChange', {
+    strings: ["Cosemetics", "Pasteries"],
+    speed: 200,
+    breakLines: false,
+    autoStart: true
+});
